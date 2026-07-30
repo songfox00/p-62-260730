e191a977de9feba1467b567c1f0b94c7152fe3eb
songfox00 <songfox00@gmail.com>
2026-07-30 11:49:58 +0900

    init, ddl-auto, show-sql

---
+++ src/main/java/com/example/demo/domain/wiseSaying/entity/WiseSaying.java
@@ -0,0 +1,19 @@
+package com.example.demo.domain.wiseSaying.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Setter
+@Getter // 어노테이션. 표식(자바, 프레임워크, IDE)
+@Entity
+@NoArgsConstructor
+public class WiseSaying {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //auto_increment
+    private int id;
+    private String content;
+    private String author;
+}
