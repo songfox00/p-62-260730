1b59247303949141c343a55a20808d42cbe28d8b
songfox00 <songfox00@gmail.com>
2026-08-04 09:42:30 +0900

    createDate, LastModifiedDate

--- src/main/java/com/example/demo/domain/post/entity/Post.java
+++ src/main/java/com/example/demo/domain/post/entity/Post.java
@@ -1,12 +1,12 @@
 package com.example.demo.domain.post.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -14,19 +14,22 @@ import java.time.LocalDateTime;
 @Getter // 어노테이션. 표식(자바, 프레임워크, IDE)
 @Entity
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) //auto_increment
     private int id;
     private String title;
     private String body;
+
+    @CreatedDate
     private LocalDateTime createDate;
+
+    @LastModifiedDate
     private LocalDateTime modifyDate;
 
     public Post(String title, String body){
         this.title = title;
         this.body = body;
-        createDate=LocalDateTime.now();
-        modifyDate=createDate;
     }
 }
